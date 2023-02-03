9424cced107cd13228f43dbe5cf9485d55c79fb4
Giovanni Di Gregorio <g.digregorio@bitrocket.dev>
2023-02-03 14:28:16 +0100

    feat(general): theme

---
+++ utils/Theme.js
@@ -0,0 +1,64 @@
+export const Theme = {
+  light: {
+    background: 'white',
+    navbar: '#202226',
+    text: 'rgba(0,0,0,.87)',
+    field: {
+      background: {
+        disabled: '#f8f8f8',
+        base: '#f8f8f8',
+      },
+      placeholder: '#72767d',
+      color: '#202226',
+      label: '#202226',
+      disabled: '#b0b0b0',
+      inactive: '#f8f8f8',
+    },
+    button: {
+      background: '#cfd7ff',
+      color: '#4e57ef',
+      disabled: {
+        background: '#cfd7ff5c',
+        bmissionstyle: 'none',
+        color: '#20222640',
+      },
+    },
+    card: {
+      background: '#f8f8f8',
+      color: 'black',
+      border: '1px solid black',
+      hover: 'lightgrey',
+    },
+  },
+  dark: {
+    background: '#171c25',
+    navbar: '#202226',
+    text: 'white',
+    field: {
+      background: {
+        base: '#40444b',
+        disabled: '#40444b5c',
+      },
+      placeholder: '#72767d',
+      color: '#e1e1e1',
+      label: '#a6a6a6',
+      disabled: '#707070',
+      inactive: '#37393f3d',
+    },
+    button: {
+      background: '#4e57ef',
+      color: '#cfd7ff',
+      disabled: {
+        background: '#4e56ef51',
+        bmissionstyle: 'none',
+        color: '#e1e1e149',
+      },
+    },
+    card: {
+      background: '#20222640',
+      color: '#fff',
+      border: '1px solid #fff',
+      hover: '#72767d',
+    },
+  },
+};
