3748f4206d5375479d0570e480e9be897e85010d
Daniele Maione <d.maione@bitrocket.dev>
2023-02-08 11:42:27 +0100

    add and remove contact on imageSelection

--- src/@navigation-guest/ImageSelectionScreen/ImageSelection.tsx
+++ src/@navigation-guest/ImageSelectionScreen/ImageSelection.tsx
@@ -5,10 +5,27 @@ import * as ImagePicker from 'react-native-image-picker';
 import {Button} from './components/Button';
 import {Response} from './components/Response';
 import {Title} from './components/Title';
+import Contacts from 'react-native-contacts';
 
 export const includeExtra = true;
 
 export const ImageSelection = () => {
+  var newPerson = {
+    emailAddresses: [
+      {
+        label: 'work',
+        email: 'c.palermo@bitrocket.dev',
+      },
+    ],
+    phoneNumbers: [
+      {
+        label: 'mobile',
+        number: '3339821302',
+      },
+    ],
+    familyName: 'Palermo',
+    givenName: 'Cristian',
+  };
   const [response, setResponse] = React.useState<any>(null);
 
   const onButtonPress = React.useCallback(
@@ -25,6 +42,15 @@ export const ImageSelection = () => {
     [],
   );
 
+  const onClickDelete = () => {
+    Contacts.getContactsByEmailAddress(newPerson.emailAddresses[0].email).then(
+      contact => Contacts.deleteContact(contact[0]),
+    );
+  };
+  const onClickAdd = () => {
+    Contacts.openContactForm(newPerson);
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <Title>Image Picker</Title>
@@ -37,6 +63,14 @@ export const ImageSelection = () => {
               </Button>
             );
           })}
+          <Button key={newPerson.emailAddresses[0].email} onPress={onClickAdd}>
+            Aggiungi Contatto
+          </Button>
+          <Button
+            key={newPerson.emailAddresses[0].email}
+            onPress={onClickDelete}>
+            Rimuovi Contatto
+          </Button>
         </View>
         <Response>{response}</Response>
 
